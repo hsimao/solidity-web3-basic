67f0c7f084e96d286e273a7d1e0f4c309683b4d9
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-02-26 15:07:21 +0800

    feat: test setMessage

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -27,4 +27,12 @@ describe("Inbox", () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, INITIAL_MESSAGE);
   });
+
+  it("can change the message", async () => {
+    const newMessage = "newMessage";
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+
+    assert.equal(message, newMessage);
+  });
 });
