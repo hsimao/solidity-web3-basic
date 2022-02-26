a3f02703962e50ef56118cb523ee64e25ddc3660
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-02-26 14:55:08 +0800

    feat: test get message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,6 +6,7 @@ const { interface, bytecode } = require("../compile");
 const web3 = new Web3(ganache.provider());
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = "Hi there!";
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -13,12 +14,17 @@ beforeEach(async () => {
 
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
   });
 });
